00cda21cbbd4e566ab1813cfbfdfcaff8bf27e49
Suchith Kumar G M <suchithkumargm@gmail.com>
2023-09-22 23:18:34 +0530

    added date and time to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import logo from '../../assets/images/logo.png';
@@ -6,7 +6,47 @@ import photo from '../../assets/images/photo.jpeg';
 import icons from '../../assets/icons/icons.js';
 import './Header.css';
 
+//function to get the current date and time 
+const formatDateTime = (date) => {
+	const dayOptions = { weekday: 'short' };
+	const day = date.toLocaleDateString(undefined, dayOptions);
+
+	//time format
+	const timeOptions = {
+		hour: '2-digit',
+		minute: '2-digit',
+		hour12: true,
+	};
+	const time = date.toLocaleTimeString(undefined, timeOptions);
+
+	//date format
+	const dateOptions = {
+		day: '2-digit',
+		month: '2-digit',
+		year: 'numeric',
+	};
+	const formattedDate = date.toLocaleDateString(undefined, dateOptions);
+
+	return { day, time, formattedDate };
+};
+
 const Header = () => {
+
+	const [currentDateTime, setCurrentDateTime] = useState(new Date());
+
+	// Update the current date and time every second
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+			setCurrentDateTime(new Date());
+		}, 1000);
+
+		// Clean up the interval set
+		return () => clearInterval(intervalId);
+	}, []);
+
+	//destructure the date and time
+	const { day, time, formattedDate } = formatDateTime(currentDateTime);
+
 	return (
 		<header>
 			<div className='brand'>
@@ -15,9 +55,9 @@ const Header = () => {
 			</div>
 			<div className='content'>
 				<p><i className='text-yellow'>{icons['Notification']}</i></p>
-				<div className='text-yellow'>
-						<p>date </p>
-						<p>time</p>
+				<div className='text-yellow font-bold'>
+					<p>{formattedDate} </p>
+					<p>{day} {time}</p>
 				</div>
 				<div>
 					<img className='photo' src={photo} alt='photo' />
